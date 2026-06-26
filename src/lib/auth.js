fe44b9f2856c2f6ffbca686e2ae669b3f4b55f3e
fardinSanjida <fputul71@gmail.com>
2026-06-26 20:41:55 +0600

    deploy env set

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -31,12 +31,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-  },
+  // socialProviders: {
+  //   google: {
+  //     clientId: process.env.GOOGLE_CLIENT_ID,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  //   },
+  // },
   user: {
     additionalFields: {
       role: {
